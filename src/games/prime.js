0c3ae93a999299827669fbf217df489143b3f960
Korolev Michael <korolev.m.c@gmail.com>
2022-12-04 21:20:31 +0200

    megasquash

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,25 +1,25 @@
 import { print, readline } from '../cli.js';
 import { runGame } from '../index.js';
-import { getRandomInt, isEven } from '../utils.js';
+import { getRandomInt, isPrime } from '../utils.js';
 
 // Game mechanic constants
 const INTERVAL_FROM = 1;
-const INTERVAL_TO = 50;
+const INTERVAL_TO = 100;
 
 // UI Constants
-const DESCRIPTION = 'Answer "yes" if the number is even, otherwise answer "no".';
+const DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 const RIGHT_ANSWER_MSG = 'Correct!';
 const USER_YES = 'yes';
 const USER_NO = 'no';
 
 const printGameOverMessage = (userAnswer, rightAnswer, userName) => print(`
-"${userAnswer}" is wrong answer ;(. Correct answer was "${rightAnswer}".
+'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.
 Let's try again, ${userName}!
 `);
 
 const playRound = (userName) => {
   const randomInt = getRandomInt(INTERVAL_FROM, INTERVAL_TO);
-  const rightAnswer = isEven(randomInt) ? USER_YES : USER_NO;
+  const rightAnswer = isPrime(randomInt) ? USER_YES : USER_NO;
 
   print(`Question: ${randomInt}`);
 
